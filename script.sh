1c2df89739c1ed04f6da666ce3584338c2ef36eb
Radha Kavuloori <radhakrishna.kavuloori@skillsoft.com>
2023-07-29 17:52:02 -0400

    1st commit script

---
+++ script.sh
@@ -0,0 +1,11 @@
+$ apt install curl apt-transport-https gnupg
+
+$ apt install locales
+$ locale-gen en_US.UTF-8
+$ echo "LANG=en_US.UTF-8" > /etc/default/locale
+
+$ echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 22.04 main"| \
+   tee /etc/apt/sources.list.d/zammad.list > /dev/null
+
+$ apt update
+$ apt install zammad
