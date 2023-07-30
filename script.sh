052f181b347ac77e464a926a99e422d948c18185
Radhakrishnaskillsoft <138715245+Radhakrishnaskillsoft@users.noreply.github.com>
2023-07-29 21:15:19 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-$ apt install curl apt-transport-https gnupg
+apt install curl apt-transport-https gnupg
 
-$ apt install locales
-$ locale-gen en_US.UTF-8
-$ echo "LANG=en_US.UTF-8" > /etc/default/locale
+apt install locales
+locale-gen en_US.UTF-8
+echo "LANG=en_US.UTF-8" > /etc/default/locale
 
-$ echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 22.04 main"| \
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 22.04 main"| \
    tee /etc/apt/sources.list.d/zammad.list > /dev/null
 
-$ apt update
-$ apt install zammad
+apt update
+apt install zammad
